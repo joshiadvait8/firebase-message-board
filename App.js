68583fcc0eafe49ad872e92e0f94ab7a76bbf447
joshiadvait8 <joshiadvait8@gmail.com>
2019-05-31 13:09:15 +0530

    :rocket:

---
+++ App.js
@@ -0,0 +1,159 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  FlatList
+} from "react-native";
+import { Input, Card, Button, Icon } from "native-base";
+import * as firebase from "firebase";
+
+//fireabse config
+const firebaseConfig = {
+  apiKey: "AIzaSyD07za5hBoYVROHeNtq8uaMQrfFfZEMiR8",
+  authDomain: "reactbootcamp-9c44a.firebaseapp.com",
+  databaseURL: "https://reactbootcamp-9c44a.firebaseio.com",
+  projectId: "reactbootcamp-9c44a",
+  storageBucket: "reactbootcamp-9c44a.appspot.com",
+  messagingSenderId: "853141223186",
+  appId: "1:853141223186:web:03f2d2e91d183945"
+};
+firebase.initializeApp(firebaseConfig);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      messageList: []
+    };
+  }
+  //how we can send a message to a user
+  sendMessage = message => {
+    var messageListRef = firebase.database().ref("message_list");
+
+    //push message to database
+    var newMessageRef = messageListRef.push();
+    newMessageRef.set({
+      text: message,
+      time: Date.now()
+    });
+    this.setState({
+      message: ""
+    });
+  };
+
+  //will update our constructors messagelist state
+  updateList = messageList => {
+    this.setState({
+      messageList: messageList
+    });
+  };
+
+  componentWillMount() {
+    //tricky stuff goes here
+    var messageListRef = firebase.database().ref("message_list");
+
+    //whenever entry is made into database trigger is called internally
+    messageListRef.on("value", dataSnapShot => {
+      //into callback
+      if (dataSnapShot.val()) {
+        //if something is there or not
+        //just to have this keyword trickery JS classic trick
+        var self = this;
+
+        //if node is present theere store in messagelist
+        let messageList = Object.values(dataSnapShot.val());
+        self.updateList(messageList.reverse());
+      }
+    });
+  }
+
+  render() {
+    //inverted will actually inverts data list
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Message Board</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={this.state.messageList}
+            inverted
+            keyExtractor={(item, index) => {
+              item.time.toString();
+            }}
+            renderItem={({ item }) => (
+              <Card style={styles.listItem}>
+                <Text style={styles.messageText}>{item.text}</Text>
+                <Text style={styles.timeText}>
+                  {new Date(item.time).toLocaleDateString}
+                </Text>
+              </Card>
+            )}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Input
+            onChangeText={text => {
+              this.setState({ message: text });
+            }}
+            value={this.state.message}
+            placeholder="Enter Message"
+          />
+          <Button
+            danger
+            rounded
+            icon
+            onPress={() => {
+              this.sendMessage(this.state.message);
+            }}
+          >
+            <Icon name="arrow-forward" />
+          </Button>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 25,
+    margin: 2,
+    backgroundColor: "#01CBC6"
+  },
+  header: {
+    backgroundColor: "#2B2B52",
+    alignItems: "center",
+    height: 40,
+    justifyContent: "center"
+  },
+  headerText: {
+    paddingHorizontal: 10,
+    color: "#FFF",
+    fontSize: 20
+  },
+  listContainer: {
+    flex: 1,
+    padding: 5
+  },
+  listItem: {
+    padding: 10
+  },
+  messageText: {
+    fontSize: 20
+  },
+  timeText: {
+    fontSize: 10
+  },
+  inputContainer: {
+    flexDirection: "row",
+    padding: 5,
+    borderWidth: 5,
+    borderRadius: 15,
+    borderColor: "#2B2B52",
+    color: "#fff"
+  }
+});
